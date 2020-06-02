c4fc7901a41b34ecaca9b25e77521c1ae855080b
yagodml <yagoskor@gmail.com>
2020-06-02 14:49:39 -0300

    imports

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,9 @@
     <router-view />
   </div>
 </template>
+
+<style lang="scss">
+@import './main.scss';
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap');
+</style>
